7beb690f6fe9ec0ca4ee0ad75bd55b378517de55
prem-charan <premxcharan@gmail.com>
2026-06-23 23:47:14 +0530

    gate ws-server using the token

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,25 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (ws) => {
-    // ws.on('error', console.error);
+wss.on('connection', function (ws, request) {
+    const url = request.url;
+    if (!url) {
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") ?? "";
 
-    ws.on('message', (data) => {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if (!decoded || !(decoded as JwtPayload).userId) {
+        ws.close();
+        return;
+    }
+    
+    ws.on("message", function message(data) {
         ws.send("pong");
-    })
+    });
 });
 console.log("ws server running on port 8080");
